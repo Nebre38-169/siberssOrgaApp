388c1c26d0f94a6c8ba8b0cf54aaa05fae6cde23
deathtracks <antoine.doste@hotmail.fr>
2021-04-27 09:17:01 +0200

    Prepared for production

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  apiKey : '=HBvRe*EI`X=r]@Z!vh*yZcO2)1xC',
+  baseUrl : {
+    //TODO : add the correct url for api
+    base : '',
+    boquette : '/boquette',
+    rotance : '/rotance',
+    channel : '/channel',
+    posts : '/posts',
+    auth : '/auth'
+  }
 };
